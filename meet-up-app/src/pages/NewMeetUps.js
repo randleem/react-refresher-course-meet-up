ac38576a63ca1f31c5d65cec452d123e60f13937
randleem <erandle2211@gmail.com>
2022-06-29 18:41:05 +0100

    lesson 43 - addign history.replace

--- meet-up-app/src/pages/NewMeetUps.js
+++ meet-up-app/src/pages/NewMeetUps.js
@@ -1,6 +1,9 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
+import { useHistory } from "react-router-dom";
 
 function NewMeetUpsPage() {
+  const history = useHistory();
+
   function handleAdddMeetup(meetupData) {
     fetch(
       "https://react-getting-started-608ec-default-rtdb.europe-west1.firebasedatabase.app/meetups.json",
@@ -11,7 +14,9 @@ function NewMeetUpsPage() {
           "Content-Type": "application/json",
         },
       }
-    );
+    ).then(() => {
+      history.replace("/");
+    });
   }
 
   return (
